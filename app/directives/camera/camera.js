2994f17bd3e0ac063829b2d7747268315252365b
Yair Cohen <mysmallfish@gmail.com>
2013-09-17 04:44:37 -0400

    update

---
+++ app/directives/camera/camera.js
@@ -0,0 +1,25 @@
+(function(){
+ 
+    function createCamera() {
+    return {
+      restrict: 'A',
+      require: 'ngModel',
+      link: function(scope, elm, attrs, ctrl) {
+         elm.on('click', function() {
+             
+            navigator.camera.getPicture(function (imageURI) {
+               scope.$apply(function() {
+                  ctrl.$setViewValue(imageURI);
+               });
+            }, function (err) {
+               ctrl.$setValidity('error', false);
+            }/*, { quality: 50, destinationType: Cordova.Camera.DestinationType.FILE_URI }*/);
+            
+         });
+      }
+   };
+    }
+    
+   angular.module("bank").directive("camera", createCamera);
+   
+})();
